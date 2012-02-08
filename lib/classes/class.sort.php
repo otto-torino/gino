830f168a0afc6f98e746edee437729a3dc211cf4
Marco Guidotti <marco.guidotti@otto.to.it>
2012-02-08 14:42:30 +0100

    E' stata aggiunta la possibilità di indicare il nome del campo in riferimento al quale viene effettuato l'ordinamento

--- lib/classes/class.sort.php
+++ lib/classes/class.sort.php
@@ -2,10 +2,11 @@
 /*
 Form:
 
-$sort = new sort(array('table'=>calendarEvent::$_tbl_selection, 'instance'=>$this->_instance));
+$sort = new sort(array('table'=>$table, 'instance'=>$this->_instance));
+$htmlList = new htmlList(array("numItems"=>count($items), "separator"=>false, "id"=>'priorityList'));
 ...[ciclo]...
 $link_sort = $sort->link();
-$GINO .= $htmlList->item($name, array($link_sort), '', true, $content, "id$evt->id", "sortable");
+$GINO .= $htmlList->item($name, array($link_sort), '', true, $content, "id$item->id", "sortable");
 ...[/ciclo]...
 $GINO .= $sort->jsLib($this->_home."?pt[{$this->_className}-actionUpdateOrder]");
 
@@ -19,7 +20,7 @@ Action:
 		$items = explode(",", $order);
 		$i=1;
 		foreach($items as $item) {
-			$sort = new sort(array('id'=>$item, 'instance'=>$this->_instance, 'table'=>calendarEvent::$_tbl_selection, 'field_id'=>'reference'));
+			$sort = new sort(array('id'=>$item, 'instance'=>$this->_instance, 'table'=>$table, 'field_id'=>'reference'));
 			$sort->priority = $i;
 			$sort->updateDbData();
 			$i++;
@@ -59,12 +60,29 @@ class sort {
 			'table'=>null,
 			'instance'=>null,
 			'aggregator'=>null,
+			'field_aggregator'=>'aggregator',
 			'ul_id'=>'priorityList',
 			'link_class'=>'orderPriority',
 			'link_style'=>"float:left;width:20px;height:20px;background:url('img/ico_sort.gif');cursor:move;margin-right:3px;"
 		);
 	private $_priority;
 
+	/**
+	 * 
+	 * @param array $data
+	 * 		id					integer		valore ID del record
+	 * 		field_id			string		nome del campo ID
+	 * 		field_sort			string		nome del campo che contiene i valori di ordinamento
+	 * 		table				string		nome della tabella
+	 * 		instance			integer		valore ID dell'istanza
+	 * 		aggregator			integer		valore del campo sul quale viene effettuato l'ordinamento 
+	 * 										nel caso in cui l'ordinamento sia relativo a un sottoinsieme di elementi
+	 * 		field_aggregator	string		nome del campo del sottoinsieme di ordinamento
+	 * 		ul_id				string		div ID
+	 * 		link_class			string		classe del div che rappresenta il collegamento all'icona di ordinamento
+	 * 		link_style			string		stile del div che rappresenta il collegamento all'icona di ordinamento
+	 * return void
+	 */
 	function __construct($data = array()) {
 	
 		foreach($data as $k=>$v) {
@@ -95,7 +113,9 @@ class sort {
 		return "<div class=\"".$this->_p['link_class']."\" style=\"".$this->_p['link_style']."\"></div>";
 	}
 	
-	public function jsLib($path_action){
+	public function jsLib($path_action, $options=array()){
+		
+		$reference = array_key_exists('reference', $options) ? "&ref=".$options['reference'] : '';
 		
 		$GINO = "<script>";
 		$GINO .= "function message() { alert('"._("Ordinamento effettuato con successo")."')}";
@@ -107,7 +127,7 @@ class sort {
 						var order = this.serialize(1, function(element, index) {
 							return element.getProperty('id').replace('id', '');
 						}).join(',');
-						ajaxRequest('post', '$path_action', 'order='+order+'&tbl={$this->_p['table']}', null, {'callback':message});
+						ajaxRequest('post', '$path_action', 'order='+order+'$reference&tbl={$this->_p['table']}', null, {'callback':message});
        					}
 
 			})";
@@ -121,7 +141,7 @@ class sort {
 		
 		$where = $this->_p['instance'] ? " WHERE instance='{$this->_p['instance']}'" : '';
 		if($where AND $this->_p['aggregator'])
-			$where .= " AND aggregator='{$this->_p['aggregator']}'";
+			$where .= " AND {$this->_p['field_aggregator']}='{$this->_p['aggregator']}'";
 		
 		$query = "SELECT MAX({$this->_p['field_sort']}) as m FROM ".$this->_p['table'].$where;
 		$a = $db->selectquery($query);
