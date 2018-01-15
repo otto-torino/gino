1a5a729afa3ee698e6fe5505dc43d786653f076f
Marco Guidotti <guidottim@gmail.com>
2018-01-15 15:02:07 +0100

    Update content pages

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
 <footer class="text-center">
-	Otto Srl | <a href="page/view/privacy-cookie/">Privacy - Cookie</a> | <a href="admin">Area amministrativa</a>
+	Otto Srl | <a href="https://opensource.org/licenses/MIT" rel="external">The MIT license</a> | <a href="page/view/privacy-cookie/">Privacy - Cookie</a> | <a href="admin">Area amministrativa</a>
 	<div class="credits"><a target="_blank" href="http://www.otto.to.it"><img style="margin-left: 20px; width: 30px;" src="img/otto_credits.jpg" /></a></div>
 </footer>
