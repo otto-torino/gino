8c0321209bd1d0486db72727acfcb4b4eb99ba84
Marco Guidotti <guidottim@gmail.com>
2018-11-16 17:14:25 +0100

    Output changes in case exceptions are called

--- lib/classes/http/class.ResponseServerError.php
+++ lib/classes/http/class.ResponseServerError.php
@@ -3,7 +3,7 @@
  * @file class.ResponseServerError.php
  * @brief Contiene la definizione ed implementazione della classe Gino.Http.ResponseServerError
  *
- * @copyright 2014 Otto srl (http://www.opensource.org/licenses/mit-license.php) The MIT License
+ * @copyright 2014-2018 Otto srl (http://www.opensource.org/licenses/mit-license.php) The MIT License
  * @author marco guidotti guidottim@gmail.com
  * @author abidibo abidibo@gmail.com
  */
@@ -15,23 +15,34 @@ use \Gino\Loader;
 /**
  * @brief Subclass di Gino.Http.Response per gestire risposte a seguito di errori interni (code 500)
  *
- * @copyright 2014 Otto srl (http://www.opensource.org/licenses/mit-license.php) The MIT License
+ * @copyright 2014-2018 Otto srl (http://www.opensource.org/licenses/mit-license.php) The MIT License
  * @author marco guidotti guidottim@gmail.com
  * @author abidibo abidibo@gmail.com
  */
 class ResponseServerError extends Response {
 
+    private $_add_content;
+    
     /**
      * @brief Costruttore
      * @param array $kwargs
-     * @return istanza di Gino.Http.ResponseServerError
+     * @return void
      */
     function __construct(array $kwargs = array()) {
 
         parent::__construct('', $kwargs);
 
         $this->setStatus(500, 'Internal Server Error');
-
+        $this->_add_content = null;
+    }
+    
+    /**
+     * @brief Imposta contenuti aggiuntivi
+     * @param string $string
+     */
+    public function setAddContent($string) {
+        
+        $this->_add_content = $string;
     }
 
     /**
@@ -43,7 +54,12 @@ class ResponseServerError extends Response {
 
         // per evitare errori che causano un ciclo infinito
         try {
-            $document = Loader::load('Document', array(\Gino\App\Sysfunc\sysfunc::page500()));
+            $content = \Gino\App\Sysfunc\sysfunc::page500();
+            if($this->_add_content) {
+                $content .= $this->_add_content;
+            }
+            $document = Loader::load('Document', array($content));
+            
             ob_start();
             echo $document->render();
             ob_end_flush();
@@ -51,9 +67,11 @@ class ResponseServerError extends Response {
         catch(\Exception $e) {
             ob_start();
             echo \Gino\App\Sysfunc\sysfunc::page500();
+            if($this->_add_content) {
+                echo $this->_add_content;
+            }
             ob_end_flush();
         }
-
     }
 
 }
