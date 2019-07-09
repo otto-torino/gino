f28e6a2ba6272b59f6a3b19422fe2ded1e0e69e1
Marco Guidotti <guidottim@gmail.com>
2019-07-09 10:46:42 +0200

    Css style in Modal and comments

--- dbgino.sql
+++ dbgino.sql
@@ -1,19 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 10, 2013 at 05:19 PM
--- Server version: 5.5.34-0ubuntu0.13.04.1
--- PHP Version: 5.4.9-4ubuntu2.3
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `dbgino`
@@ -1233,7 +1217,3 @@ CREATE TABLE IF NOT EXISTS `sys_tag_taggeditem` (
   PRIMARY KEY (`id`),
   KEY `index_tagid` (`tag_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
